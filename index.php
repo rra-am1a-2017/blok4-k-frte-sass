af2f5f75f565e5cedfdf2d348095184f1e6bb87b
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2018-05-25 16:17:42 +0200

    einde les vrij-25-05-2018

--- index.php
+++ index.php
@@ -1,89 +1,127 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/introstyle.css">
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <header class="container-fluid">
-      <p>Navbar</p>
-    </header>
-    <main class="container">
-      <article>Carousel</article>
-      <article class="row">
-        <section class="col-6 col-md-12 col-lg-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/blauw.png" alt="blauw">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/introstyle.css">
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <header class="container-fluid">
+    <p>Navbar</p>
+  </header>
+  <main class="container">
+    <article class="row">
+      <section class="col-12">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-50 h-50 mx-auto" src="./images/kwikkwekkwak.png" alt="First slide">
             </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje met kleur
+            <div class="carousel-item">
+              <img class="d-block w-50 h-50 mx-auto" src="./images/dagobertduck.jpg" alt="Second slide">
             </div>
-          </div>
-        </section> 
-        <section class="col-6 col-md-3 col-lg-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/bruin.png" alt="bruin">
-            </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje
-            </div>
-          </div>
-        </section> 
-        <section class="col-6 col-md-3 col-lg-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/geel.png" alt="geel">
-            </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje
+            <div class="carousel-item">
+              <img class="d-block w-50 h-50 mx-auto" src="./images/magica.png" alt="Third slide">
             </div>
           </div>
-        </section> 
-        <section class="col-6 col-md-3 col-lg-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/goud.png" alt="goud">
-            </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje
-            </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+      </section>
+    </article>
+
+    <article class="row">
+      <section class="col-6 col-md-12 col-lg-3">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/blauw.png" alt="blauw">
           </div>
-        </section> 
-        <section class="col-6 col-md-3 col-lg-12">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/grijs.png" alt="grijs">
-            </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje
-            </div>
+          <div class="card-footer text-muted">
+            Dit is een blokje met kleur
           </div>
-        </section> 
-        <section class="col-6 col-md-12 col-lg-12">
-          <div class="card text-center">
-            <div class="card-body">
-              <img src="./images/groen.png" alt="groen">
-            </div>
-            <div class="card-footer text-muted">
-              Dit is een blokje
-            </div>
+        </div>
+      </section>
+      <section class="col-6 col-md-3 col-lg-3">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/bruin.png" alt="bruin">
           </div>
-        </section>       
-      </article>   
-    </main>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  </body>
+          <div class="card-footer text-muted">
+            Dit is een blokje
+          </div>
+        </div>
+      </section>
+      <section class="col-6 col-md-3 col-lg-3">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/geel.png" alt="geel">
+          </div>
+          <div class="card-footer text-muted">
+            Dit is een blokje
+          </div>
+        </div>
+      </section>
+      <section class="col-6 col-md-3 col-lg-3">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/goud.png" alt="goud">
+          </div>
+          <div class="card-footer text-muted">
+            Dit is een blokje
+          </div>
+        </div>
+      </section>
+      <section class="col-6 col-md-3 col-lg-12">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/grijs.png" alt="grijs">
+          </div>
+          <div class="card-footer text-muted">
+            Dit is een blokje
+          </div>
+        </div>
+      </section>
+      <section class="col-6 col-md-12 col-lg-12">
+        <div class="card text-center">
+          <div class="card-body">
+            <img src="./images/groen.png" alt="groen">
+          </div>
+          <div class="card-footer text-muted">
+            Dit is een blokje
+          </div>
+        </div>
+      </section>
+    </article>
+  </main>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
