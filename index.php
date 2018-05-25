5197b620006795482a56bb5d17fada6b9fe75ef5
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2018-05-25 14:56:05 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/introstyle.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header class="container-fluid">
+      <p>Navbar</p>
+    </header>
+    <main class="container">
+      <article>Carousel</article>
+      <article class="row">
+        <section class="col-6 col-md-12 col-lg-3">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/blauw.png" alt="blauw">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje met kleur
+            </div>
+          </div>
+        </section> 
+        <section class="col-6 col-md-3 col-lg-3">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/bruin.png" alt="bruin">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje
+            </div>
+          </div>
+        </section> 
+        <section class="col-6 col-md-3 col-lg-3">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/geel.png" alt="geel">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje
+            </div>
+          </div>
+        </section> 
+        <section class="col-6 col-md-3 col-lg-3">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/goud.png" alt="goud">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje
+            </div>
+          </div>
+        </section> 
+        <section class="col-6 col-md-3 col-lg-12">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/grijs.png" alt="grijs">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje
+            </div>
+          </div>
+        </section> 
+        <section class="col-6 col-md-12 col-lg-12">
+          <div class="card text-center">
+            <div class="card-body">
+              <img src="./images/groen.png" alt="groen">
+            </div>
+            <div class="card-footer text-muted">
+              Dit is een blokje
+            </div>
+          </div>
+        </section>       
+      </article>   
+    </main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
